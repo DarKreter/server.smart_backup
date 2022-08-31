017834c738d67d41a77d0e0e7f9600b971e25013
DarKreter <darkreter@gmail.com>
2022-08-31 13:18:34 +0200

    Copy subtitles, texts and posters

--- backup.sh
+++ backup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Siema!"
+source_path=/mnt/ANIME
+# source_path=./test
+dest_path=./backup/
+
+# remove existing backup
+rm -rf $dest_path/*
+
+# extension to just copy
+copy_extensions=( '*.srt' '*.ass' '*.jpg' '*.png' '*.jpeg' '*.txt' '*.md')
+# copy with folder structure
+for ext in "${copy_extensions[@]}"
+do
+    :
+   find $source_path -type f -iname $ext -exec rsync -ahv --progress --relative {} $dest_path \;
+done
