0f74139971a723ff5b076a65d1321e5c6393397e
DarKreter <darkreter@gmail.com>
2022-08-31 13:42:29 +0200

    copy metadata of library

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source_path=/mnt/ANIME
-# source_path=./test
-dest_path=./backup/
+# source_path=/home/kretes/k8d.server.smart_backup/test
+dest_path=/home/kretes/k8d.server.smart_backup/backup/
 
 # remove existing backup
 rm -rf $dest_path/*
@@ -13,5 +13,14 @@ copy_extensions=( '*.srt' '*.ass' '*.jpg' '*.png' '*.jpeg' '*.txt' '*.md')
 for ext in "${copy_extensions[@]}"
 do
     :
-   find $source_path -type f -iname $ext -exec rsync -ahv --progress --relative {} $dest_path \;
+   find $source_path -type f -iname $ext -exec rsync -a --progress --relative {} $dest_path \;
+done
+
+# extension to smart copy metadata
+copy_extensions=( '*.mkv' '*.mp4' '*.mka')
+# copy with folder structure
+for ext in "${copy_extensions[@]}"
+do
+    :
+   find $source_path -type f -iname $ext -exec ./copyMetadata.sh $dest_path {}  \;
 done
---
+++ copyMetadata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dest_path=$1
+full_file_path=$2
+
+# echo $full_file_path
+
+# Get file directory and file name
+DIR="$(dirname "${full_file_path}")" ; FILE="$(basename "${full_file_path}")"
+
+# Make sure path exists
+mkdir -p "$dest_path"/"$DIR"
+# Create file with identic name but inside are only metadata
+exiftool "$full_file_path" > "$dest_path"/"$full_file_path"
